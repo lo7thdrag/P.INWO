1641b0c75d41e2353216aadf082b717344c10e9b
lo7thdrag <princeachmad@gmail.com>
2024-11-21 10:34:59 +0700

    update tote

--- Source/LibTTT/LibForms/SimulatorForm/ufrmToteDisplay.pas
+++ Source/LibTTT/LibForms/SimulatorForm/ufrmToteDisplay.pas
@@ -4,10 +4,12 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.ComCtrls;
 
 type
   TfrmToteDisplay = class(TForm)
+    ListView1: TListView;
+    Panel1: TPanel;
   private
     { Private declarations }
   public
